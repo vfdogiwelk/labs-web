806b0a4abc98f7295177b0a966d8110489aa4fdb
{PRKOL} <ilya.ivanyna@gmail.com>
2026-02-11 20:18:05 +0200

    lab 12

---
+++ lr12/script.js
@@ -0,0 +1,27 @@
+// Завдання 2
+console.log("Іванина");
+
+// Завдання 6
+let firstName = "Ілля";
+let surname = "Іванина";
+let group = "КІ-406";
+let birthYear = 2005;
+let isMarried = false;
+
+console.log("Тип firstName: " + typeof firstName);
+console.log("Тип surname: " + typeof surname);
+console.log("Тип group: " + typeof group);
+console.log("Тип birthYear: " + typeof birthYear);
+console.log("Тип isMarried: " + typeof isMarried);
+
+console.log("Number: birthYear = " + birthYear);
+console.log("Boolean: isMarried = " + isMarried);
+console.log("String: firstName = " + firstName);
+console.log("String: surname = " + surname);
+console.log("String: group = " + group);
+
+let nullVar = null;
+let undefinedVar;
+
+console.log("Тип nullVar: " + typeof nullVar);
+console.log("Тип undefinedVar: " + typeof undefinedVar);
